5491096f51c50bfcb2dac4a9adc035cd9dede0bd
hudk <hudk2019@163.com>
2019-10-31 10:13:42 +0800

    前端静态文件

---
+++ data-dictionary-web/src/main/resources/static/login/precache-manifest.b151a04925e35ad8f61f532b1d166ec3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d32a95db13444c08719f0a4f17964373",
+    "url": "./index.html"
+  },
+  {
+    "revision": "42e516fb802ccca0b038",
+    "url": "./static/css/2.6b67ac3d.chunk.css"
+  },
+  {
+    "revision": "e854b4c1efbc447d2c49",
+    "url": "./static/css/main.f3c3c322.chunk.css"
+  },
+  {
+    "revision": "42e516fb802ccca0b038",
+    "url": "./static/js/2.f18a4fc6.chunk.js"
+  },
+  {
+    "revision": "e854b4c1efbc447d2c49",
+    "url": "./static/js/main.ca58655b.chunk.js"
+  },
+  {
+    "revision": "2291e0bc3d042150b2f5",
+    "url": "./static/js/runtime-main.2e5f2085.js"
+  }
+]);
